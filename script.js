be0df65ac8b9f550d8ee5132c8da7e363e2d76f0
Jess Dorman <pickupthesky@gmail.com>
2021-02-15 22:12:31 -0500

    Add more time of day options for bg

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 // GETS HOUR OF DAY
-const hour = new Date().getHours();
+let hour = new Date().getHours();
 
 
-// DISPLAYS BG IMAGE VALUE
-// const body = document.querySelector('body');
-// const background = window.getComputedStyle(body);
-// console.log( background.getPropertyValue('background-image') );
+// CHANGE TO SIMULATE DIFFERENT TIMES OF DAY
+// 0-3 night
+// 4-6 dawn
+// 7-10 morning
+// 11-17 day
+// 18-19 sunset
+// 20-23 night
+
+// hour = 20;
 
 
-// DETERMINES DAY OR NIGHT BG
+// DETERMINES BG IMAGE
 const style = document.createElement('style');
-if (hour >= 19 || hour <= 7) {
+if (0 <= hour && hour <= 3) {
     style.innerHTML = `body {background-image: url("img/sdv_night.png");}`;
-} else {
+} else if (4 <= hour && hour <= 6)  {
+    style.innerHTML = `body {background-image: url("img/sdv_dawn.png");}`;
+} else if (7 <= hour && hour <= 10)  {
+    style.innerHTML = `body {background-image: url("img/sdv_morning.png");}`;
+} else if (11 <= hour && hour <= 17)  {
     style.innerHTML = `body {background-image: url("img/sdv_day.png");}`;
+} else if (18 <= hour && hour <= 19)  {
+    style.innerHTML = `body {background-image: url("img/sdv_sunset.png");}`;
+} else {
+    style.innerHTML = `body {background-image: url("img/sdv_night.png");}`;
 }
 
 
